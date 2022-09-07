9d5a2b7e3640565ca21e245081f9216c1aa62247
lobservateur <BrunetA@LAPTOP-BNTE06E4>
2022-09-07 17:24:24 +0200

    DDD

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import axios from "axios";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
   const [data, setData] = useState({
@@ -44,8 +46,36 @@ export default function Home() {
       <div>
         <h1>Bienvenue sur Infracoop</h1>
       </div>
-
       <main className={styles.main}>
+      <h2> Les véhicules disponibles à la location :</h2>
+        <div class="Carousel">
+          <Carousel>
+            <div>
+                <img src="/BMWCE04Img.jpg" alt="image1"/>
+                <p className="legend">Scooter BMW-CE 04</p>
+
+            </div>
+            <div>
+                <img src="/BMWCEVOImg.jpg" alt="image2" />
+                <p className="legend">Scooter BMW-CEV0</p>
+
+            </div>
+            <div>
+                <img src="/F45iImg.jpg" alt="image3"/>
+                <p className="legend">Scooter F45i</p>
+
+            </div>
+            <div>
+                <img src="/TilgreenTmaxImg.jpg" alt="image4"/>
+                <p className="legend">Scooter Tilgreen T-max</p>
+
+            </div>
+            <div>
+                <img src="/YamahaE01Img.jpeg" alt="image5"/>
+                <p className="legend">Scooter Yamaha E-01</p>
+            </div>
+          </Carousel>
+        </div>
         EMAIL : {data.email} | PASSWORD : {data.password}
       </main>
     </div>
